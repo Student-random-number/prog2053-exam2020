639076c1557faa0f924ec8aab50ef6d491130108
Student <76154463+Student-random-number@users.noreply.github.com>
2020-12-17 12:40:46 +0100

    Update edit-user.js

--- oppgave/www/js/components/edit-user.js
+++ oppgave/www/js/components/edit-user.js
@@ -6,8 +6,29 @@ class EditUser extends LitElement {
       user: { type: Object }
     };
   }
+  
+  render() {
+  return html`   
+    <div> 
+      <label for="fname">First name:</label>
+      <input type="text" id="newName" name="newName" value="${this.firstName}"><br><br>
+      <label for="lname">Last name:</label>
+      <input type="text" id="newLaName" name="newLaName" value="${this.lastName}"><br><br>
+      <label for="lname">Current password:</label>
+      <input type="text" id="cPassword" name="cPassword" value="${this.}"><br><br>
+      <label for="lname">New password:</label>
+      <input type="text" id="newPassword" name="newPassword" value="${this.}"><br><br>
+      <input type="submit" value="Submit" @click=${this.uppdateUser}>
+    </div>
+  }
 
+  uppdaterUser(e){
+
+    fetch(`${api/updateUser.php}`
+        ).then( res => res.json()
+        ).then(data => {
+        }
+  }
   // din kode her
-
 }
 customElements.define('edit-user', EditUser);
